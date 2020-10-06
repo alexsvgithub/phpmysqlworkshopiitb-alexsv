dbde63cbb95aad4e5633fb5679f1efdf1d23ea47
alexsvgithub <53620960+alexsvgithub@users.noreply.github.com>
2020-10-06 21:20:06 +0530

    second day commit

---
+++ Day1/array1.php
@@ -0,0 +1,24 @@
+
+<?php
+  $Week= array("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+
+echo $Week[0];
+echo"<br>";
+
+echo $Week[1];
+echo"<br>";
+
+echo $Week[2];
+echo"<br>";
+
+echo $Week[3];
+echo"<br>";
+
+echo $Week[4];
+echo"<br>";
+
+echo $Week[5];
+echo"<br>";
+
+echo $Week[6];
+ ?>
---
+++ Day1/vowel.php
@@ -0,0 +1,17 @@
+<?php
+$cha="a";
+switch ($cha) {
+  case 'a': echo "$cha is a vowel";
+    break;
+    case 'e': echo "$cha is a vowel";
+      break;
+      case 'i': echo "$cha is a vowel";
+        break;
+        case 'o': echo "$cha is a vowel";
+          break;
+          case 'u': echo "$cha is a vowel";
+            break;
+    default: echo "$cha is a consonent";
+}
+
+?>
